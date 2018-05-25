4703060104748f269505804512510c0bc5951520
ALLYIO <zekanisalex@gmail.com>
2018-05-25 14:26:12 +0200

    grid reform

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -46,10 +46,10 @@ export default {
 }
 </script>
 
-<style>
-
-
+<style scoped>
+h2,h4{color: whitesmoke;}
 </style>
+
 
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,39 @@
 <div class="img" v-bind:style="{ backgroundImage: 'url(' + image + ')' }">
 
     <my-banner />
+    <b-container>
+
+
+<div class="row" style="padding-top: 5%;padding-bottom: 5%;">
+<div class="col-md-4">
 <my-grid/>
+</div> <!-- col // -->
+<div class="col-md-4">
+<my-grid/>	
+</div> <!-- col // -->
+<div class="col-md-4">
+	<my-grid/>
+</div> <!-- col // -->
+</div> <!-- row.// -->
+
+<div class="row" style="padding-top: 5%;padding-bottom: 5%;">
+<div class="col-md-4">
+	<my-grid/>
+</div> <!-- col // -->
+<div class="col-md-4">
+	<my-grid/>
+</div> <!-- col // -->
+<div class="col-md-4">
+<my-grid/>
+</div> <!-- col // -->
+</div> <!-- row.// -->
+
+
+
+
+
+        
+    </b-container>
     
 </div>
 
@@ -34,5 +66,39 @@
     .img {
     max-width: 100%;
     max-height: 100%;
-    }
+}
+
+.card-product .img-wrap {
+  
+    border-radius: 3px 3px 0 0;
+    overflow: hidden;
+    position: relative;
+    height: 220px;
+    text-align: center;
+}
+.card-product .img-wrap img {
+    max-height: 100%;
+    max-width: 100%;
+    object-fit: cover;
+}
+.card-product .info-wrap {
+    overflow: hidden;
+    padding: 15px;
+    border-top: 1px solid #eee;
+   
+}
+.card-product .bottom-wrap {
+    padding: 15px;
+    border-top: 1px solid #eee;
+}
+
+.label-rating { margin-right:10px;
+    color: #333;
+    display: inline-block;
+    vertical-align: middle;
+}
+
+.card-product .price-old {
+    color: #999;
+}
 </style>
