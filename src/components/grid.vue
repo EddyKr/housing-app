419a25b3a7bf3553c8594a9bc8034d8757ea704c
Minda Bartosz <mindabartosz@gmail.com>
2018-06-23 17:56:33 +0200

    Fix property grid.

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -1,22 +1,131 @@
 <template>
-	<figure class="card card-product">
-		<div class="img-wrap"><img src="https://s9.postimg.org/tupxkvfj3/image.jpg">
-		</div>
-		<figcaption class="info-wrap">
-				<h5 class="title">Title</h5>
-				<p class="desc">Some small description goes here</p>
-				<div class="rating-wrap">
-					<div class="label-rating">132 views</div>
-				</div> 
-		</figcaption>
+  <div>
+    <div class="row" id="rowTiles">
+        <div class="col-md-4">
+            <figure class="card card-product">
+                <div class="img-wrap"><img src="https://media.pararius.nl/image/PR0001462000/PR0001462707/image/jpeg/180x300/RotterdamKralingseweg-ba4d_16.jpg">
+                </div>
+                <figcaption class="info-wrap">
+                        <h5 class="title">Kralingen-Crooswijk, Rotterdam</h5>
+                        <p class="desc">For rent luxury and modern three-room corner apartment fully furnished with fantastic view.</p>
+                        <div class="rating-wrap">
+                            <div class="label-rating">17 views</div>
+                        </div>
+                </figcaption>
 
-		<div class="bottom-wrap">
-			<a href="" class="btn btn-sm btn-primary float-right">Take a look</a>	
-			<div class="price-wrap h5">
-				<span class="price-new">500 eu.</span> <del class="price-old"> 750 eu.</del>
-			</div> <!-- price-wrap.// -->
-		</div> <!-- bottom-wrap.// -->		
-	</figure>
+                <div class="bottom-wrap">
+                    <a href="" class="btn btn-sm btn-primary float-right">Take a look</a>
+                    <div class="price-wrap h5">
+                        <span class="price-new">1850 eu. </span> <del class="price-old"> 2000 eu.</del>
+                    </div> <!-- price-wrap.// -->
+                </div> <!-- bottom-wrap.// -->
+            </figure>
+        </div>
+        <div class="col-md-4">
+            <figure class="card card-product">
+                <div class="img-wrap"><img src="https://media.pararius.nl/image/PR0001462000/PR0001462709/image/jpeg/180x300/AmsterdamNieuweLeliestraat-24c7_1.jpg">
+                </div>
+                <figcaption class="info-wrap">
+                        <h5 class="title">Stadsdeel Centrum, Amsterdam</h5>
+                        <p class="desc">Very nice recently built ground floor apartment in the nicest part of Amsterdam: The Jordaan area.</p>
+                        <div class="rating-wrap">
+                            <div class="label-rating">134 views</div>
+                        </div>
+                </figcaption>
+
+                <div class="bottom-wrap">
+                    <a href="" class="btn btn-sm btn-primary float-right">Take a look</a>
+                    <div class="price-wrap h5">
+                        <span class="price-new">1650 eu.</span>
+                    </div> <!-- price-wrap.// -->
+                </div> <!-- bottom-wrap.// -->
+            </figure>
+        </div>
+        <div class="col-md-4">
+            <figure class="card card-product">
+                <div class="img-wrap"><img src="https://media.pararius.nl/image/PR0001462000/PR0001462712/image/jpeg/180x300/AmsterdamVanLeijenberghlaan-64fd_1.jpg">
+                </div>
+                <figcaption class="info-wrap">
+                        <h5 class="title">Stadsdeel Zuid, Amsterdam</h5>
+                        <p class="desc">Brand new on the market is this luxury modern, completely renovated and fully furnished 2 bedroom rental apartment.</p>
+                        <div class="rating-wrap">
+                            <div class="label-rating">94 views</div>
+                        </div>
+                </figcaption>
+
+                <div class="bottom-wrap">
+                    <a href="" class="btn btn-sm btn-primary float-right">Take a look</a>
+                    <div class="price-wrap h5">
+                        <span class="price-new">2800 eu. </span> <del class="price-old"> 3000 eu.</del>
+                    </div> <!-- price-wrap.// -->
+                </div> <!-- bottom-wrap.// -->
+            </figure>
+        </div>
+    </div>
+
+    <div class="row" id="rowTiles" >
+        <div class="col-md-4">
+            <figure class="card card-product">
+                <div class="img-wrap"><img src="https://media.pararius.nl/image/PR0001342000/PR0001342139/image/jpeg/180x300/RotterdamVroesenlaan-1a3e_1.jpg">
+                </div>
+                <figcaption class="info-wrap">
+                        <h5 class="title">Noord, Rotterdam</h5>
+                        <p class="desc">Stairwell, entrance, hall, very airy spacious living room with access to the balcony, kitchen with a big 6-burner gas cookplate.</p>
+                        <div class="rating-wrap">
+                            <div class="label-rating">132 views</div>
+                        </div>
+                </figcaption>
+
+                <div class="bottom-wrap">
+                    <a href="" class="btn btn-sm btn-primary float-right">Take a look</a>
+                    <div class="price-wrap h5">
+                        <span class="price-new">1500 eu.</span>
+                    </div> <!-- price-wrap.// -->
+                </div> <!-- bottom-wrap.// -->
+            </figure>
+        </div>
+        <div class="col-md-4">
+            <figure class="card card-product">
+                <div class="img-wrap"><img src="https://media.pararius.nl/image/PR0001462000/PR0001462689/image/jpeg/180x300/EindhovenMethusalemplein-c9e4_14.jpg">
+                </div>
+                <figcaption class="info-wrap">
+                        <h5 class="title">Stadsdeel Stratum, Eindhoven</h5>
+                        <p class="desc">Recently renovated 3-room apartment located on a unique location in the center of Eindhoven.</p>
+                        <div class="rating-wrap">
+                            <div class="label-rating">194 views</div>
+                        </div>
+                </figcaption>
+
+                <div class="bottom-wrap">
+                    <a href="" class="btn btn-sm btn-primary float-right">Take a look</a>
+                    <div class="price-wrap h5">
+                        <span class="price-new">1195 eu. </span> <del class="price-old"> 1295 eu.</del>
+                    </div> <!-- price-wrap.// -->
+                </div> <!-- bottom-wrap.// -->
+            </figure>
+        </div>
+        <div class="col-md-4">
+            <figure class="card card-product">
+                <div class="img-wrap"><img src="https://media.pararius.nl/image/PR0001462000/PR0001462570/image/jpeg/180x300/EindhovenSchans-9e12_1.jpg">
+                </div>
+                <figcaption class="info-wrap">
+                        <h5 class="title">Stadsdeel Tongelre, Eindhoven</h5>
+                        <p class="desc">For rent: spacious house with built-in storage, 5 bedrooms, 2 bathrooms, large living room and open kitchen.</p>
+                        <div class="rating-wrap">
+                            <div class="label-rating">215 views</div>
+                        </div>
+                </figcaption>
+
+                <div class="bottom-wrap">
+                    <a href="" class="btn btn-sm btn-primary float-right">Take a look</a>
+                    <div class="price-wrap h5">
+                        <span class="price-new">1425 eu.</span>
+                    </div> <!-- price-wrap.// -->
+                </div> <!-- bottom-wrap.// -->
+            </figure>
+        </div>
+    </div>
+  </div>
 </template>
 
 
