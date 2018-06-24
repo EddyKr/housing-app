9367a3b4ea54b482fe8965976e5f67ce7ff27f81
Minda Bartosz <mindabartosz@gmail.com>
2018-06-24 23:27:58 +0200

    Update appointments subpage.

--- src/views/Appointment.vue
+++ src/views/Appointment.vue
@@ -3,179 +3,55 @@
     <b-row>
         <b-col cols="4" offset="1" class="mt-4">
             <h1>Pending requests</h1>
-            <div class="my-5">
+            <div class="my-5" v-for="(pending_meeting,index) in this.pending_meetings" v-bind:key="pending_meeting.id">
                 <b-row class="meeting-request">
                     <b-col cols="12" class="d-flex align-items-center my-3">
                         <img src="https://www.w3schools.com/w3css/img_lights.jpg" class="profile-picture rounded-circle mr-3">
                         <div class="d-flex flex-column">
                             <div class="d-flex mt-3">
                                 <span class="mr-3"> Request from: </span>
-                                <span> John Potato </span>
+                                <span> {{pending_meeting.user.first_name}} {{pending_meeting.user.last_name}} </span>
                             </div>
                             <div class="d-flex mt-3">
                                 <span class="mr-3"> Meeting time: </span>
-                                <span> 23/06/18 18:00 </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting location: </span>
-                                <span> Rachelsmolen 1, Eindhoven </span>
+                                <span> {{pending_meeting.meeting_time}} </span>
                             </div>
                             <div class="d-flex mt-3">
                                 <span class="mr-3"> Property link: </span>
-                                <router-link to="/property">Proeprty Link</router-link>
+                                <router-link :to="{name:'post', params:{id: pending_meeting.post_id}}">Proeprty Link</router-link>
                             </div>
                         </div>
                         <div class="d-flex flex-column ml-auto">
-                            <b-btn variant="success" class="mb-3">Accept</b-btn>
-                            <b-btn variant="danger">Decline</b-btn>
-                        </div>
-                    </b-col>
-                </b-row>
-            </div>
-            <div class="my-5">
-                <b-row class="meeting-request">
-                    <b-col cols="12" class="d-flex align-items-center my-3">
-                        <img src="https://www.w3schools.com/w3css/img_lights.jpg" class="profile-picture rounded-circle mr-3">
-                        <div class="d-flex flex-column">
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Request from: </span>
-                                <span> John Potato </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting time: </span>
-                                <span> 23/06/18 18:00 </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting location: </span>
-                                <span> Rachelsmolen 1, Eindhoven </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Property link: </span>
-                                <router-link to="/property">Proeprty Link</router-link>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-column ml-auto">
-                            <b-btn variant="success" class="mb-3">Accept</b-btn>
-                            <b-btn variant="danger">Decline</b-btn>
-                        </div>
-                    </b-col>
-                </b-row>
-            </div>
-            <div class="my-5">
-                <b-row class="meeting-request">
-                    <b-col cols="12" class="d-flex align-items-center my-3">
-                        <img src="https://www.w3schools.com/w3css/img_lights.jpg" class="profile-picture rounded-circle mr-3">
-                        <div class="d-flex flex-column">
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Request from: </span>
-                                <span> John Potato </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting time: </span>
-                                <span> 23/06/18 18:00 </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting location: </span>
-                                <span> Rachelsmolen 1, Eindhoven </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Property link: </span>
-                                <router-link to="/property">Proeprty Link</router-link>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-column ml-auto">
-                            <b-btn variant="success" class="mb-3">Accept</b-btn>
-                            <b-btn variant="danger">Decline</b-btn>
+                            <b-btn variant="success" class="mb-3" v-on:click="acceptMeeting(pending_meeting.id)">Accept</b-btn>
+                            <b-btn variant="danger" v-on:click="declineMeeting(pending_meeting.id)">Decline</b-btn>
                         </div>
                     </b-col>
                 </b-row>
             </div>
         </b-col>
-        <b-col cols="4" offset="1" class="mt-4">
+
+        <b-col cols="4" offset="2" class="mt-4">
             <h1>Accepted requests</h1>
-            <div class="my-5">
+            <div class="my-5" v-for="(accepted_meeting,index) in this.accepted_meetings" v-bind:key="accepted_meeting.id">
                 <b-row class="meeting-request">
                     <b-col cols="12" class="d-flex align-items-center my-3">
                         <img src="https://www.w3schools.com/w3css/img_lights.jpg" class="profile-picture rounded-circle mr-3">
                         <div class="d-flex flex-column">
                             <div class="d-flex mt-3">
                                 <span class="mr-3"> Meeting with: </span>
-                                <span> John Potato </span>
+                                <span> {{accepted_meeting.user.first_name}} {{accepted_meeting.user.last_name}} </span>
                             </div>
                             <div class="d-flex mt-3">
                                 <span class="mr-3"> Meeting time: </span>
-                                <span> 23/06/18 18:00 </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting location: </span>
-                                <span> Rachelsmolen 1, Eindhoven </span>
+                                <span> {{accepted_meeting.meeting_time}} </span>
                             </div>
                             <div class="d-flex mt-3">
                                 <span class="mr-3"> Property link: </span>
-                                <router-link to="/property">Proeprty Link</router-link>
+                                <router-link :to="{name:'post', params:{id: accepted_meeting.post_id}}">Proeprty Link</router-link>
                             </div>
                         </div>
                         <div class="d-flex flex-column ml-auto">
-                            <b-btn variant="success" class="mb-3">Private chat</b-btn>
-                            <b-btn variant="primary">Update</b-btn>
-                        </div>
-                    </b-col>
-                </b-row>
-            </div>
-            <div class="my-5">
-                <b-row class="meeting-request">
-                    <b-col cols="12" class="d-flex align-items-center my-3">
-                        <img src="https://www.w3schools.com/w3css/img_lights.jpg" class="profile-picture rounded-circle mr-3">
-                        <div class="d-flex flex-column">
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Request from: </span>
-                                <span> John Potato </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting time: </span>
-                                <span> 23/06/18 18:00 </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting location: </span>
-                                <span> Rachelsmolen 1, Eindhoven </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Property link: </span>
-                                <router-link to="/property">Proeprty Link</router-link>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-column ml-auto">
-                            <b-btn variant="success" class="mb-3">Private chat</b-btn>
-                            <b-btn variant="primary">Update</b-btn>
-                        </div>
-                    </b-col>
-                </b-row>
-            </div>
-            <div class="my-5">
-                <b-row class="meeting-request">
-                    <b-col cols="12" class="d-flex align-items-center my-3">
-                        <img src="https://www.w3schools.com/w3css/img_lights.jpg" class="profile-picture rounded-circle mr-3">
-                        <div class="d-flex flex-column">
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Request from: </span>
-                                <span> John Potato </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting time: </span>
-                                <span> 23/06/18 18:00 </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Meeting location: </span>
-                                <span> Rachelsmolen 1, Eindhoven </span>
-                            </div>
-                            <div class="d-flex mt-3">
-                                <span class="mr-3"> Property link: </span>
-                                <router-link to="/property">Proeprty Link</router-link>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-column ml-auto">
-                            <b-btn variant="success" class="mb-3">Private chat</b-btn>
-                            <b-btn variant="primary">Update</b-btn>
+
                         </div>
                     </b-col>
                 </b-row>
@@ -186,13 +62,59 @@
 </template>
 
 <script>
-    import TopNavBar from '@/components/top-nav-2.vue'
-export default {
-    components: {
-        TopNavBar
-    }
+    import ApiHelper from '../apiHelper.js'
 
-}
+    export default {
+      data(){
+          return {
+              pending_meetings: [],
+              accepted_meetings: []
+          }
+      },
+      created: function() {
+        var self = this;
+        this.getMeetingsInfo();
+      },
+      methods: {
+        getMeetingsInfo:function() {
+            ApiHelper.apiPost(this, null, 'meetings/pending').then(function(response){
+              this.pending_meetings = response.body.data.pendingMeetings;
+              console.log(response.body.data.pendingMeetings);
+            });
+
+            ApiHelper.apiPost(this, null, 'meetings/accepted').then(function(response){
+              this.accepted_meetings = response.body.data.acceptedMeetings;
+              console.log(response.body.data.acceptedMeetings);
+            });
+        },
+        acceptMeeting:function(meeting_id) {
+            console.log(meeting_id);
+
+            let data = {
+                meeting_id: meeting_id
+            };
+
+            ApiHelper.apiPost(this, data, 'meetings/accept').then(function(response){
+              console.log(response.body.data);
+            });
+
+            location.reload();
+        },
+        declineMeeting:function(meeting_id) {
+            console.log(meeting_id);
+
+            let data = {
+                meeting_id: meeting_id
+            };
+
+            ApiHelper.apiPost(this, data, 'meetings/decline').then(function(response){
+              console.log(response.body.data);
+            });
+
+            location.reload();
+        }
+      }
+    }
 </script>
 
 <style>
@@ -206,6 +128,10 @@ export default {
         font-size: 18px;
         color: white;
         border-radius: 10px;
+    }
+
+    .row {
+        margin-right: 0;
     }
 
 </style>
